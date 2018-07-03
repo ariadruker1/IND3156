5bc7f40e27dfceb26c7432e936d0da3e3ee08c5e
ariadruker1 <40701200+ariadruker1@users.noreply.github.com>
2018-07-03 10:42:37 -0700

    Add files via upload
    learning how to use go language - will be used to play and try different things in the language and see how outputs are impacted.

---
+++ hellotest.go
@@ -0,0 +1,7 @@
+// <-- is how to comment in go
+package main // any time I want to run something executable
+
+import "fmt" //functions same as c's print and scanf
+func main() {
+	fmt.Println("Hello WOrld!")
+}
